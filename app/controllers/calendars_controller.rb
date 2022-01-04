f02deba7ccb28caaf8f19c274e5bd8559c62fb82
marrrkk <92919150+marrrkk@users.noreply.github.com>
2022-01-04 21:53:01 +0900

    Revert "Second training"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:Plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -31,17 +31,10 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
-      plans.each do |plan|f
+      plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = # wdayメソッドを用いて取得した数値
-      # if #「wday_numが7以上の場合」という条件式
-        #wday_num = wday_num -7
-      # end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[Date.today.wday]}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
